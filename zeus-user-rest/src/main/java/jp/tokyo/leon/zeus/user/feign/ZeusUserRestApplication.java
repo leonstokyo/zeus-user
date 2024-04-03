9e709ea0c456b08bf90d8ebbbc9d2ea5bfa8ebc8
longtao.guan <longtao.guan@okcoin.jp>
2024-04-03 14:11:23 +0900

    init

---
+++ zeus-user-rest/src/main/java/jp/tokyo/leon/zeus/user/feign/ZeusUserRestApplication.java
@@ -0,0 +1,14 @@
+package jp.tokyo.leon.zeus.user.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author longtao.guan
+ */
+@SpringBootApplication
+public class ZeusUserRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusUserRestApplication.class);
+    }
+}
