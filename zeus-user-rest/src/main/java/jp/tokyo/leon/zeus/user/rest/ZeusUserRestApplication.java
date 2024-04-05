9da269caaded74ae16f0513a045846330685a671
leonseki <leonstokyo@gmail.com>
2024-04-05 22:58:08 +0900

    gateway

--- zeus-user-rest/src/main/java/jp/tokyo/leon/zeus/user/rest/ZeusUserRestApplication.java
+++ zeus-user-rest/src/main/java/jp/tokyo/leon/zeus/user/rest/ZeusUserRestApplication.java
@@ -2,12 +2,14 @@ package jp.tokyo.leon.zeus.user.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author longtao.guan
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"jp.tokyo.leon.zeus.user.rest", "jp.tokyo.leon.zeus.user.service"})
 public class ZeusUserRestApplication {
     public static void main(String[] args) {
